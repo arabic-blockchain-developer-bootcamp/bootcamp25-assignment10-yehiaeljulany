d291309716fdbaa82c728b7674e084f5ff46a93f
Omar Osman <omarosman23@aucegypt.edu>
2025-03-20 00:57:55 +0200

    Create Assignment10.t.sol

---
+++ test/Assignment10.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+
+contract Assignment10Test is Test {
+    function testAlwaysPass() public {
+        // This is a gift assignment :)
+        assertTrue(true);
+    }
+}
