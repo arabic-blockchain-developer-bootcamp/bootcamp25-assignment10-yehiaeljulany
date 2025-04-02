13dab4a2bf84d87904200932214badc9d0b0f1f6
yehiaeljulany <98744657+yehiaeljulany@users.noreply.github.com>
2025-04-02 16:45:43 +0200

    Update Assignment10.sol

--- src/Assignment10.sol
+++ src/Assignment10.sol
@@ -3,19 +3,20 @@ pragma solidity ^0.8.13;
 
 contract Assignment10 {
     // Use `constant` for values that will not change
-    uint256 public FEE = 100;
+    uint256 public constant FEE = 100;
 
     // Pack variables in optimized order
     uint128 x;
-    uint256 y;
     uint128 z;
+    uint256 y;
+    
 
 
     // Use `private` instead of `public` for variables not accessed externally
-    uint256 public internalCounter;
+    uint256 private internalCounter;
 
     // Use `external` instead of `public` for functions called externally only
-    function setValue(uint128 _x) public {
+    function setValue(uint128 _x) external {
         x = _x;
     }
 }
